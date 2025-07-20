72777102ff6b7f642efbf922743dbf5e2530999b
chal580lp <68598714+chal580lp@users.noreply.github.com>
2025-07-20 18:05:54 +1200

    Delete NpcExtensions.kt

--- NpcExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.recursive.quester.framework.extension
-
-import com.runemate.game.api.hybrid.entities.Npc
-import com.runemate.game.api.hybrid.local.Camera
-
-fun Npc?.isInteractable(): Boolean {
-    return this != null && (this.visibility > 99 || Camera.turnTo(this)) && this.position?.isReachable == true
-}
