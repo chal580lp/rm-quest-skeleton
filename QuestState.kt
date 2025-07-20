ab6b75a73d1921bf6dba6df97897b513ac5c0f3d
chal580lp <68598714+chal580lp@users.noreply.github.com>
2025-07-20 18:06:23 +1200

    Delete QuestState.kt

--- QuestState.kt
+++
@@ -1,9 +0,0 @@
-package com.recursive.quester.framework.execution
-
-data class QuestState(
-    val questName: String,
-    val currentStep: String,
-    val progress: Int,
-    val isComplete: Boolean,
-    val failureCount: Int
-)
